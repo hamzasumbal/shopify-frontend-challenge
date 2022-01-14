56ac723c2381b017f23bcb5002c7417272f94001
hamzasumbal <msumbal.bee15seecs@seecs.edu.pk>
2022-01-13 19:24:43 -0500

    header added

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Header.module.css'
+
+const Header = ()=>{
+
+
+    return<header className={classes.header}>
+        <span>Spacestagram</span>
+        <h3>Image-sharing from the final frontier</h3>
+    </header>
+
+
+};
+
+export default Header;
