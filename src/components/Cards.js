21f3c21b2db567ddd75b7b9e5eb26cf854610c05
hamzasumbal <msumbal.bee15seecs@seecs.edu.pk>
2022-01-13 21:16:50 -0500

    loading spinner added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,38 +1,45 @@
-import React, {useEffect, useState} from 'react';
-import Card from './Card';
-import classes from './Cards.module.css';
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+import classes from "./Cards.module.css";
+import { Spinner } from "react-bootstrap";
 
+const Cards = () => {
+  const [spaceData, setSpaceData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-const Cards = ()=>{
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.nasa.gov/planetary/apod?api_key=9wChezTPPYejHAnvpDCq6h3vbxSE58vagZEJVDLW&count=8"
+        );
+        const data = await response.json();
+        console.log(data);
+        setSpaceData(data);
+        setLoading(false);
+      } catch {
+        console.log("Error");
+      }
+    };
 
-    const [spaceData, setSpaceData] = useState([]);
+    getData();
+  }, []);
 
-    useEffect(()=>{
-
-        const getData = async ()=>{
-    
-            try{
-            const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=9wChezTPPYejHAnvpDCq6h3vbxSE58vagZEJVDLW&count=8')
-            const data = await response.json();
-            console.log(data);
-            setSpaceData(data);
-            }catch{
-                console.log("Error")
-            }
-        }
-    
-        getData();
-        
-
-    },[])
-
-
-return<section className={classes.cardsContainer}> 
-{spaceData.map((item,index)=>{
-    return <Card key={index} data = {item}/>
-})}
-</section>
-
-}
+  return (
+    <section className={classes.cardsContainer}>
+      {loading ? (
+        <div className={classes.spinner}>
+        <Spinner animation="border" variant="light" />
+        </div>
+      ) : (
+        <>
+          {spaceData.map((item, index) => {
+            return <Card key={index} data={item} />;
+          })}
+        </>
+      )}
+    </section>
+  );
+};
 
 export default Cards;
